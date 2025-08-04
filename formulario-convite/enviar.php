c9d788e6b658c287e2da2411fff552f9ace6d085
gwilhermealm <guilhermealmeidze@gmail.com>
2025-08-04 13:23:04 -0300

    iniciar testes
    testar php

---
+++ formulario-convite/enviar.php
@@ -0,0 +1,17 @@
+<?php
+$nome =addslashes()($_POST['nome']);
+$email = addslashes() ($_POST['email']);
+
+$para = "guilhermeaalmeidze@gmail.com";
+$assunto = "Confirmaçao de presença";
+
+$corpo = "Nome:".$nome."\n"."email:".$email;
+
+$cabecalho = "From: gwilhermejw@gmail.com "."\n". "reply-to: ".$email."\n". "X-Mailer: PHP/".phpversion();
+
+if(mail($para, $assunto, $corpo, $cabecalho)){
+    echo ("Email enviado com sucesso!");
+} else {
+    echo ("Erro ao enviar o email.");
+}
+?>
