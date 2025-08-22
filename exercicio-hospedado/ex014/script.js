5e666f0fc4f653d6e374c723c529297554ddb921
gwilhermealm <guilhermealmeidze@gmail.com>
2025-08-22 12:20:47 -0300

    subir projeto

--- exercicio-hospedado/ex014/script.js
+++ exercicio-hospedado/ex014/script.js
@@ -5,7 +5,7 @@ function carregar() {
 
     var data = new Date();
     var hora = data.getHours();
-    //var hora = 12
+     //var hora = 18
     if (hora == 1){
          msg.innerHTML = `Agora são ${hora} hora.`
     } else {
@@ -14,19 +14,19 @@ function carregar() {
    
     if (hora >=0 && hora < 12){
         //bom dia
-        bdn.innerHTML = 'Bom dia!'
+        bdn.innerHTML = 'Bom dia! &#x1F324 '
         document.body.style.backgroundImage = "url('bg-manha.jpg')"
        
     }
     else if (hora >=12 && hora < 18){
         //boa tarde
-       bdn.innerHTML = 'Boa tarde!'
+       bdn.innerHTML = 'Boa tarde! &#x2600'
         document.body.style.backgroundImage = "url('bg-tarde.jpg')"
        
     }
     else {
         //boa noite
-        bdn.innerHTML = 'Boa noite!'
+        bdn.innerHTML = 'Boa noite! &#x1F319'
         document.body.style.backgroundImage = "url('bg-noite.jpg')"
     }
 
